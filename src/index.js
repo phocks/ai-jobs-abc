5514d0f5f02c6216626b2b96d2c51d1808cb02fb
Joshua Byrd <phocks@gmail.com>
2017-08-03 16:39:40 +1000

    Your bar and compasisons

--- src/index.js
+++ src/index.js
@@ -60,6 +60,8 @@ const app = new Vue({
     percentMoreSusceptible: '',
     lessTasks: [],
     moreTasks: [],
+    shareURL: encodeURIComponent(window.location.href),
+    shareText: '',
   }
 });
 
@@ -115,15 +117,17 @@ function selectGroup (selectedGroupData, jobTitle) {
     app.groupTitle = selectedGroupData.groupTitle;
 
     // Check if percentages are the same and redraw
-    if (app.percentLessSusceptible === selectedGroupData.percentLessSusceptible &&
-        app.percentMoreSusceptible === selectedGroupData.percentMoreSusceptible) {
-          app.$refs.pieLess.drawPie(selectedGroupData.percentLessSusceptible);
-          app.$refs.pieMore.drawPie(selectedGroupData.percentMoreSusceptible);
-        } 
-    else {
-      app.percentLessSusceptible = selectedGroupData.percentLessSusceptible;
+    // if (app.percentLessSusceptible === selectedGroupData.percentLessSusceptible &&
+    //     app.percentMoreSusceptible === selectedGroupData.percentMoreSusceptible) {
+    //       app.$refs.pieLess.drawPie(selectedGroupData.percentLessSusceptible);
+    //       app.$refs.pieMore.drawPie(selectedGroupData.percentMoreSusceptible);
+    //     } 
+    // else {
       app.percentMoreSusceptible = selectedGroupData.percentMoreSusceptible;
-    } 
+      app.percentLessSusceptible = selectedGroupData.percentLessSusceptible;
+    // } 
+
+    app.shareText = encodeURIComponent(selectedGroupData.percentMoreSusceptible + "% of my job is susceptible to automation. What's yours?");
 
     // Clear the lists for next search
     app.lessTasks = [];
@@ -136,6 +140,8 @@ function selectGroup (selectedGroupData, jobTitle) {
     app.moreTasks.push(selectedGroupData.taskMoreAffected1);
     if (selectedGroupData.taskMoreAffected2)
       app.moreTasks.push(selectedGroupData.taskMoreAffected2);
+
+    renderYourBarToComparison(app.percentMoreSusceptible);
 
     // Render the comparison chart
     if (app.groupTitle) {
@@ -274,9 +280,7 @@ Vue.component('waffle-chart', {
 
 Vue.component('barcode-chart', {
   props: ['yourJobPercent', 'highlightPercent'],
-  template: `<div class="barcode-chart"></div>
-  <div class="more-key">More susceptible <span class="arrow">&rarr;</span></div> 
-    <div class="less-key"><span class="arrow">&larr;</span> Less susceptible</div>`,
+  template: `<div class="barcode-chart"></div>`,
   mounted () {
     this.drawBarcode(this.yourJobPercent, this.highlightPercent);
   },
@@ -411,21 +415,187 @@ Vue.component('barcode-chart', {
 });
 
 
+Vue.component('automation-comparison', {
+  props: ['yourJobPercent'],
+  template: `<div class="automation-comparison"></div>`,
+  mounted () {
+    this.drawCharts(this.yourJobPercent);
+  },
+  watch: {
+    yourJobPercent: function (changedValue) {
+      this.drawCharts(changedValue);
+    }
+  },
+  methods: {
+    drawCharts (yourJobPercent) {
+      // A D3 chart comparison of job automation
+      // const comparisonChart = d3.select('#automation-comparison-chart');
+      // const automationList = d3.select('#automation-list');
+
+      // Get rid of the barcode chart if one is already there
+      d3.select(this.$el).selectAll("svg").remove();
+      d3.select(this.$el).selectAll("div").remove();
+
+      const automationComparison = d3.select(this.$el)
+
+      const data = jobs.automationData
+        .sort(function (a, b) {
+          return d3.ascending(a.groupTitle, b.groupTitle);
+        });
+
+      const chartWidth = '100%',
+        chartHeight = 24,
+        barWidth = 2, // chartWidth / 100;
+        barColor = 'rgba(0, 0, 0, 0.1)';
+
+      const highlightBarWidth = 3;
+      const highlightBarHeight = 28;
+      const highlightBarColor = 'rgba(255, 159, 0, 1.0)';
+
+      const yourBarWidth = 3,
+        yourBarHeight = 32,
+        yourBarColor = 'rgba(195, 51, 127, 1.0)';
+
+      const chartScale = d3.scaleLinear()
+          .domain([0, 100])
+          .range([0, 100]); // fallback to percentage as x indicator
+
+
+      const jobsInList = automationComparison.selectAll('div')
+        .data(data) 
+        .enter()
+        .append('div')
+        .attr('class', 'job-group-title')
+        .style('font-size', '15px')
+        .text(function (d) {
+          return d.groupTitle;
+        })
+
+      //   .style('cursor', 'pointer')
+      //   .on("click", (groupData) => {
+      //     selectGroup(groupData);
+      //     const searchInput = document.getElementById('job-search');
+      //     searchInput.value = groupData.groupTitle;
+      //     window.scrollTo(0,searchInput.offsetTop);
+      // });
+
+
+      const svgEl = jobsInList
+        .append('div')
+        .append('svg')
+        .attr('width', chartWidth)
+        .attr('height', chartHeight * 3);
+
+      let barcodeGroup = svgEl.append('g')
+        .attr('transform', 'translate(0, ' + chartHeight + ')');
+
+      // Render a bar that represents Your Job that you chose
+      const yourBar = barcodeGroup.append('rect')
+        .attr('width', yourBarWidth)
+        .attr('height', yourBarHeight)
+        .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
+        .style('fill', yourBarColor)
+        .attr('x', function (d) {
+          return Math.floor(chartScale(yourJobPercent)) + '%';
+        });
+
+
+      barcodeGroup.append('text')
+        .style('fill', yourBarColor)
+        .style('font-size', '11px')
+        .style('font-weight', 'bold')
+        .attr('text-anchor', 'middle')
+        .attr('x', function () {
+          return Math.floor(chartScale(yourJobPercent)) + '%';
+        })
+        .attr('y', yourBarHeight)
+        .attr('dominant-baseline', 'text-before-edge')
+        .text('Your job');
+
+      // The background bar
+      barcodeGroup.append('rect')
+        .attr('width', chartWidth)
+        .attr('height', chartHeight)
+        .style('fill', '#f4f4f4');
+
+      // Comparison transparent fill
+      barcodeGroup.append('rect')
+        .attr('width', (d) => { return chartScale(d.percentMoreSusceptible) + '%' })
+        .attr('height', chartHeight)
+        .style('fill', 'rgba(255, 155, 0, 0.15)');
+
+      // Append grey bars according to data
+      barcodeGroup.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('width', barWidth)
+        .attr('height', chartHeight)
+        .style('fill', barColor)
+        .attr('x', function (d, i) {
+          return Math.floor(chartScale(d.percentMoreSusceptible)) + "%";
+        });
+
+      // Comparison bars
+      barcodeGroup.append('rect')
+        .classed('target', true)
+        .attr('width', highlightBarWidth)
+        .attr('height', highlightBarHeight)
+        .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
+        .style('fill', highlightBarColor)
+        .attr('x', (d) => { return chartScale(d.percentMoreSusceptible) + '%' });
+
+      barcodeGroup.append('text')
+        .style('font-size', '13px')
+        .style('font-weight', '900')
+        .attr('text-anchor', 'middle')
+        .attr('x', (d) => { return chartScale(d.percentMoreSusceptible) + '%' })
+        .attr('dx', 7)
+        .attr('dy', -7)
+        .attr('dominant-baseline', 'alphabetical')
+        .text((d) => { return d.percentMoreSusceptible + '%' });
+
+
+      reorder('ascending');
+
+
+      function reorder (sortOrder) {
+        d3.selectAll('div.job-group-title')
+        .sort(function (a, b) {
+          switch (sortOrder) {
+            case "ascending":
+              // Prevent unpredictable behaviour when values are identical
+              if (a.percentLessSusceptible !== b.percentLessSusceptible)
+                return d3.ascending(a.percentLessSusceptible, b.percentLessSusceptible);
+              else
+                return a.groupTitle.localeCompare(b.groupTitle);
+              break;
+            case "descending":
+              if (a.percentLessSusceptible !== b.percentLessSusceptible)
+                return d3.descending(a.percentLessSusceptible, b.percentLessSusceptible);
+              else
+                return a.groupTitle.localeCompare(b.groupTitle);
+          }
+        })
+      };
+    },
+  },
+});
+
+
 
 // A D3 chart comparison of job automation
 const comparisonChart = d3.select('#automation-comparison-chart');
 const automationList = d3.select('#automation-list');
-const barcodeChart = d3.select('#barcode-chart');
+// const barcodeChart = d3.select('#barcode-chart');
 
-// comparisonChart.classed('hidden', true);
+comparisonChart.classed('hidden', true);
 
 
 const data = jobs.automationData
   .sort(function (a, b) {
     return d3.ascending(a.groupTitle, b.groupTitle);
   });
-
-
 
 const chartWidth = '100%',
   chartHeight = 24,
@@ -445,87 +615,35 @@ const chartScale = d3.scaleLinear()
     .range([0, 100]); // fallback to percentage as x indicator
 
 
-drawChart(barcodeChart, data, 77, 23);
+// Render a bar that represents Your Job that you chose
+function renderYourBarToComparison (yourJobPercent) {
+  d3.selectAll('.your-bar').remove();
+  d3.selectAll('.your-text').remove();
 
-
-function drawChart(d3El, data, highlightPosition, yourBarPosition) {
-  var svgEl = d3El
-    .append('svg')
-    .attr('width', chartWidth)
-    .attr('height', chartHeight * 3);
-
-  let barcodeGroup = svgEl.append('g')
-    .attr('transform', 'translate(0, ' + chartHeight + ')');
-
-  // Render a bar that represents Your Job that you chose
-  const yourBar = barcodeGroup.append('rect')
+   barcodeGroup.append('rect')
+    .classed('your-bar', true)
     .attr('width', yourBarWidth)
     .attr('height', yourBarHeight)
     .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
     .style('fill', yourBarColor)
-    .attr('x', function () {
-      return Math.floor(chartScale(yourBarPosition)) + '%';
+    .attr('x', function (d) {
+      return Math.floor(chartScale(yourJobPercent)) + '%';
     });
 
   barcodeGroup.append('text')
+  .classed('your-text', true)
     .style('fill', yourBarColor)
     .style('font-size', '11px')
     .style('font-weight', 'bold')
     .attr('text-anchor', 'middle')
     .attr('x', function () {
-      return Math.floor(chartScale(yourBarPosition)) + '%';
+      return Math.floor(chartScale(yourJobPercent)) + '%';
     })
     .attr('y', yourBarHeight)
     .attr('dominant-baseline', 'text-before-edge')
     .text('Your job');
+}
 
-  // The background bar
-  barcodeGroup.append('rect')
-    .attr('width', chartWidth)
-    .attr('height', chartHeight)
-    .style('fill', '#f4f4f4');
-
-  barcodeGroup.append('rect')
-    .attr('width', chartScale(highlightPosition) + '%')
-    .attr('height', chartHeight)
-    .style('fill', 'rgba(255, 155, 0, 0.15)');
-
-  // Append grey bars according to data
-  barcodeGroup.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('width', barWidth)
-    .attr('height', chartHeight)
-    .style('fill', barColor)
-    .attr('x', function (d, i) {
-      return Math.floor(chartScale(d.percentMoreSusceptible)) + "%";
-    });
-
-  // Render a bar for comparison
-  var highlightBar = barcodeGroup.append('rect')
-    .attr('width', highlightBarWidth)
-    .attr('height', highlightBarHeight)
-    .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
-    .style('fill', highlightBarColor)
-    .attr('x', chartScale(highlightPosition) + '%');
-
-  barcodeGroup.append('text')
-    .style('font-size', '18px')
-    .style('font-weight', 'bold')
-    .attr('text-anchor', 'middle')
-    .attr('x', function () {
-      return Math.floor(chartScale(highlightPosition)) + '%';
-    })
-    .attr('dx', 7)
-    .attr('dy', -7)
-    .attr('dominant-baseline', 'alphabetical')
-    .text(highlightPosition + '%');
-
-  barcodeChart.append('div')
-    .classed('chart-key-container', true)
-    .html('<div class="more-key">More susceptible <span class="arrow">&rarr;</span></div><div class="less-key"><span class="arrow">&larr;</span> Less susceptible</div>');
-};
 
 
 const jobsInList = automationList.selectAll('div')
@@ -555,29 +673,6 @@ const svgEl = jobsInList
 
 let barcodeGroup = svgEl.append('g')
   .attr('transform', 'translate(0, ' + chartHeight + ')');
-
-// Render a bar that represents Your Job that you chose
-const yourBar = barcodeGroup.append('rect')
-  .attr('width', yourBarWidth)
-  .attr('height', yourBarHeight)
-  .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
-  .style('fill', yourBarColor)
-  .attr('x', function (d) {
-    return Math.floor(chartScale(12)) + '%';
-  });
-
-
-barcodeGroup.append('text')
-  .style('fill', yourBarColor)
-  .style('font-size', '11px')
-  .style('font-weight', 'bold')
-  .attr('text-anchor', 'middle')
-  .attr('x', function () {
-    return Math.floor(chartScale(12)) + '%';
-  })
-  .attr('y', yourBarHeight)
-  .attr('dominant-baseline', 'text-before-edge')
-  .text('Your job');
 
 // The background bar
 barcodeGroup.append('rect')
@@ -623,6 +718,7 @@ barcodeGroup.append('text')
   .text((d) => { return d.percentMoreSusceptible + '%' });
 
 
+
 reorder('ascending');
 
 
@@ -660,8 +756,6 @@ function reorder (sortOrder) {
 
   // d3.select("button.ascending").on("click", () => { reorder('ascending') } );
   // d3.select("button.descending").on("click", () => { reorder('descending') } );
-
-  
 
 
 
@@ -746,3 +840,87 @@ function reorder (sortOrder) {
 //     }
 //   }
 // });
+
+
+
+// drawChart(barcodeChart, data, 77, 23);
+
+
+// function drawChart(d3El, data, highlightPosition, yourBarPosition) {
+//   var svgEl = d3El
+//     .append('svg')
+//     .attr('width', chartWidth)
+//     .attr('height', chartHeight * 3);
+
+//   let barcodeGroup = svgEl.append('g')
+//     .attr('transform', 'translate(0, ' + chartHeight + ')');
+
+//   // Render a bar that represents Your Job that you chose
+//   const yourBar = barcodeGroup.append('rect')
+//     .attr('width', yourBarWidth)
+//     .attr('height', yourBarHeight)
+//     .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
+//     .style('fill', yourBarColor)
+//     .attr('x', function () {
+//       return Math.floor(chartScale(yourBarPosition)) + '%';
+//     });
+
+//   barcodeGroup.append('text')
+//     .style('fill', yourBarColor)
+//     .style('font-size', '11px')
+//     .style('font-weight', 'bold')
+//     .attr('text-anchor', 'middle')
+//     .attr('x', function () {
+//       return Math.floor(chartScale(yourBarPosition)) + '%';
+//     })
+//     .attr('y', yourBarHeight)
+//     .attr('dominant-baseline', 'text-before-edge')
+//     .text('Your job');
+
+//   // The background bar
+//   barcodeGroup.append('rect')
+//     .attr('width', chartWidth)
+//     .attr('height', chartHeight)
+//     .style('fill', '#f4f4f4');
+
+//   barcodeGroup.append('rect')
+//     .attr('width', chartScale(highlightPosition) + '%')
+//     .attr('height', chartHeight)
+//     .style('fill', 'rgba(255, 155, 0, 0.15)');
+
+//   // Append grey bars according to data
+//   barcodeGroup.selectAll('rect')
+//     .data(data)
+//     .enter()
+//     .append('rect')
+//     .attr('width', barWidth)
+//     .attr('height', chartHeight)
+//     .style('fill', barColor)
+//     .attr('x', function (d, i) {
+//       return Math.floor(chartScale(d.percentMoreSusceptible)) + "%";
+//     });
+
+//   // Render a bar for comparison
+//   var highlightBar = barcodeGroup.append('rect')
+//     .attr('width', highlightBarWidth)
+//     .attr('height', highlightBarHeight)
+//     .attr('transform', 'translate(0, ' + '-' + (highlightBarHeight - chartHeight) / 2 + ')')
+//     .style('fill', highlightBarColor)
+//     .attr('x', chartScale(highlightPosition) + '%');
+
+//   barcodeGroup.append('text')
+//     .style('font-size', '18px')
+//     .style('font-weight', 'bold')
+//     .attr('text-anchor', 'middle')
+//     .attr('x', function () {
+//       return Math.floor(chartScale(highlightPosition)) + '%';
+//     })
+//     .attr('dx', 7)
+//     .attr('dy', -7)
+//     .attr('dominant-baseline', 'alphabetical')
+//     .text(highlightPosition + '%');
+
+//   barcodeChart.append('div')
+//     .classed('chart-key-container', true)
+//     .html('<div class="more-key">More susceptible <span class="arrow">&rarr;</span></div><div class="less-key"><span class="arrow">&larr;</span> Less susceptible</div>');
+// };
--- src/template.js
+++ src/template.js
@@ -10,21 +10,23 @@ module.exports = `<div class='ai-jobs'>
 <div id="app" v-if="groupTitle" v-cloak>
 
   <h2 class="job-title">{{ jobTitle }}</h2>
-  <div class="group-title">{{ groupTitle }} spend...</div>
+  <div class="group-title">{{ groupTitle }}</div>
 
-  <div class="row">
-    <div class="section-more">
+  <div v-if="false">Phew!</div>
 
-      <waffle-chart 
-        v-bind:percent="percentMoreSusceptible" 
-        section="more" 
-        ref="waffleMore">
-      </waffle-chart>
-      <!-- <pie-chart v-bind:percent="percentMoreSusceptible" ref="waffleMore"></pie-chart> -->
 
-      <p><strong>{{ percentMoreSusceptible }}%</strong> of work time on tasks that are&nbsp;<strong class="more-highlighter"> more susceptible </strong>&nbsp;to automation.</p>
+  <div class="section-more">
 
-      <div v-if="moreTasks[0]" id="tasks-more">
+    <waffle-chart 
+      v-bind:percent="percentMoreSusceptible" 
+      section="more" 
+      ref="waffleMore">
+    </waffle-chart>
+    <!-- <pie-chart v-bind:percent="percentMoreSusceptible" ref="waffleMore"></pie-chart> -->
+
+    <p><strong>{{ percentMoreSusceptible }}%</strong> of work time on tasks that are&nbsp;<strong class="more-highlighter"> more susceptible </strong>&nbsp;to automation.</p>
+
+    <div v-if="moreTasks[0]" id="tasks-more">
       <p>Some of these typical tasks are:</p>
 
       <ul id="more-tasks">
@@ -32,55 +34,77 @@ module.exports = `<div class='ai-jobs'>
           {{ item }}
         </li>
       </ul>
-
-      </div>
-
-
     </div>
 
-    <div class="section-less">
 
-      <div class="mid-text"><strong>So that means that</strong></div>
-      <div class="group-title">{{ groupTitle }} spend...</div>
-
-      <waffle-chart 
-        v-bind:percent="percentMoreSusceptible" 
-        section="less" 
-        ref="waffleLess">
-      </waffle-chart>
-
-      <p><strong>{{ percentLessSusceptible }}%</strong> of work time on tasks that are&nbsp;<strong class="less-highlighter"> less susceptible </strong>&nbsp;to automation.</p>
-
-      <div v-if="lessTasks[0]" id="tasks-less">
-        <p>Some of these typical tasks are:</p>
-
-        <ul id="less-tasks">
-          <li v-for="item in lessTasks">
-            {{ item }}
-          </li>
-        </ul>
-
-      </div>
-    </div> <!-- end section-less -->
-
-    <p>As you can see below blah blah can automate about half their work tasks.</p>
-
-    <div class="barcode-chart">
-      <barcode-chart v-bind:your-job-percent="percentMoreSusceptible" highlight-percent="55">
-      </barcode-chart>
+    <div class="Share-title">Share your result</div>
+    <div class="ShareLinks">
+      <a 
+      v-bind:href=" 'http://www.facebook.com/sharer.php?t=' + shareText +
+      '&amp;u=' + shareURL " 
+      aria-label="Share this story via facebook" 
+      class="ShareLink ShareLink--facebook" target="_blank"></a>
+      
+      <a v-bind:href=" 'http://twitter.com/share?url=' + shareURL +
+      '&amp;text=' + shareText + '&amp;via=abcnews' " 
+      aria-label="Share this story via twitter"
+      class="ShareLink ShareLink--twitter" target="_blank"></a>
+      
+      <a v-bind:href=" 'whatsapp://send?text=' + shareText + '%20' + shareURL "
+      aria-label="Share this story via whatsapp"
+      class="ShareLink ShareLink--whatsapp"></a>
+      
+      <a v-bind:href=" 'mailto:?subject=' + shareText + '&amp;body=' + shareURL "
+      aria-label="Share this story via email"
+      class="ShareLink ShareLink--email"></a>
     </div>
 
-    <p>But blah blah's susceptibility to automation is very low.</p>
 
-    <div class="barcode-chart">
-      <barcode-chart v-bind:your-job-percent="percentMoreSusceptible" highlight-percent="8">
-      </barcode-chart>
+
+  </div>
+
+  <div class="section-less">
+
+    <div class="mid-text"><strong>So that means that</strong></div>
+    <div class="group-title">{{ groupTitle }} spend...</div>
+
+    <waffle-chart 
+      v-bind:percent="percentMoreSusceptible" 
+      section="less" 
+      ref="waffleLess">
+    </waffle-chart>
+
+    <p><strong>{{ percentLessSusceptible }}%</strong> of work time on tasks that are&nbsp;<strong class="less-highlighter"> less susceptible </strong>&nbsp;to automation.</p>
+
+    <div v-if="lessTasks[0]" id="tasks-less">
+      <p>Some of these typical tasks are:</p>
+
+      <ul id="less-tasks">
+        <li v-for="item in lessTasks">
+          {{ item }}
+        </li>
+      </ul>
+
     </div>
+  </div> <!-- end section-less -->
 
-    <hr>
+  <p>As you can see below blah blah can automate about half their work tasks.</p>
 
+  <div class="barcode-chart">
+    <barcode-chart v-bind:your-job-percent="percentMoreSusceptible" highlight-percent="55">
+    </barcode-chart>
+  </div>
 
-  </div> <!-- end row -->
+  <p>But blah blah's susceptibility to automation is very low.</p>
+
+  <div class="barcode-chart">
+    <barcode-chart v-bind:your-job-percent="percentMoreSusceptible" highlight-percent="8">
+    </barcode-chart>
+  </div>
+
+  <hr>
+
+  <!-- <automation-comparison v-bind:your-job-percent="percentMoreSusceptible"></automation-comparison> -->
 
 </div> <!-- end Vue app -->
 
@@ -101,7 +125,7 @@ module.exports = `<div class='ai-jobs'>
     <div class="less-key"><span class="arrow">&larr;</span> Less susceptible</div>
   </div>  
 
-  <div id="automation-list"></div>
+   <div id="automation-list"></div> 
 </div> 
 
 </div>`
